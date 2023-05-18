facdf1d6e32736e4a60b788e148b12e5362a888d
Shuqri Shaifuddin <s10227932@connect.np.edu.sg>
2023-05-18 16:49:10 +0800

    new routing style

--- sanity/config/client-config.ts
+++
@@ -1,7 +0,0 @@
-const config = {
-    projectId: "gq3ci81f",
-    dataset: "production",
-    apiVersion: "2023-04-18",
-  }
-  
-  export default config;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,38 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
+  mode: 'jit',
   content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-
-    // Or if using `src` directory:
-    "./src/**/*.{js,ts,jsx,tsx}",
-
-    'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}'
+    './node_modules/flowbite-react/**/*.js',
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './public/**/*.html',
   ],
   theme: {
     extend: {
-      fontFamily: {
-        'rubik': ['"Rubik"', 'sans-serif'],
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+      animation: {
+        marquee: 'marquee 25s linear infinite',
+        marquee2: 'marquee2 25s linear infinite',
+      },
+      keyframes: {
+        marquee: {
+          '0%': { transform: 'translateX(0%)' },
+          '100%': { transform: 'translateX(-100%)' },
+        },
+        marquee2: {
+          '0%': { transform: 'translateX(100%)' },
+          '100%': { transform: 'translateX(0%)' },
+        },
       },
     },
   },
   plugins: [
-    require('flowbite/plugin')
+    require("flowbite/plugin")
   ],
 }
-
